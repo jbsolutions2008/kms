a3801452cf01c2abab0e8c20f16f92f56f688c0d
Telegraph Creative <server@telegraphbranding.com>
2019-03-20 09:46:30 -0500

    Init Settings Page by default

--- web/app/mu-plugins/tele-custom/config/setup.php
+++ web/app/mu-plugins/tele-custom/config/setup.php
@@ -4,7 +4,7 @@
 
 	add_filter( 'acf/settings/save_json', __NAMESPACE__ . '\\set_acf_json_save_path' );
 	add_filter( 'acf/settings/load_json', __NAMESPACE__ . '\\set_acf_json_load_path' );
-	// add_filter( 'plugins_loaded', __NAMESPACE__ . '\\initialize_acf_option_pages' );
+	add_filter( 'plugins_loaded', __NAMESPACE__ . '\\initialize_acf_option_pages' );
 
 	function set_acf_json_save_path() {
 		return TELE_LIBRARY . 'acf-json';
@@ -28,15 +28,17 @@
 				'capability' => 'edit_posts',
 				'redirect'   => true,
 				'position'   => 3,
-				'icon_url'   => 'dashicons-megaphone',
+				'icon_url'   => 'dashicons-admin-generic',
 			] );
 
+			/*
 			acf_add_options_sub_page( [
 				'page_title'  => 'General settings',
 				'menu_title'  => 'General',
 				'parent_slug' => 'site-options',
 				'icon_url'    => 'dashicons-groups',
 			] );
+			*/
 
 		}
 	}
