e4d67aeca9df729ee31f7e2488a4a30d59f3fbcd
unknown <jbsolutions2008@gmail.com>
2020-09-15 19:44:57 +0530

    Created ACF Fields for Footer Section

--- web/app/themes/kms/inc/setup.php
+++ web/app/themes/kms/inc/setup.php
@@ -47,6 +47,7 @@ if ( ! function_exists ( 'understrap_setup' ) ) {
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
 			'primary' => __( 'Primary Menu', 'understrap' ),
+			'social-menu' => __( 'Social Media Menu', 'understrap' ),
 		) );
 
 		/*
